9f0f3ae6407c608a5da46d0efdced64891152cf9
Daniil <kadaniilo@gmail.com>
2019-04-03 11:07:15 +0300

    Update application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', event => {
 
   // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
+  //   1- intercept the form submission event
   //   2- prevent the default action for that event from happening
-  //   3- use jQuery to submit an AJAX post to the form's action
-  //   4- when the AJAX post is done, display the new die roll using jQuery
+  //   3- submit an AJAX post to the form's action
+  //   4- when the AJAX post is done, display the new die roll
 
-});
+};
